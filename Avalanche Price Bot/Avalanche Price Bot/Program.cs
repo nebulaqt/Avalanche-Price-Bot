8bc7e9b8418b34dc066b29d03fb19cf0a8a11a95
nebula <jldacombe@gmail.com>
2023-01-31 08:03:47 +1300

    Upload

---
+++ Avalanche Price Bot/Avalanche Price Bot/Program.cs
@@ -0,0 +1,99 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalanche_Price_Bot
+{
+    internal class Program
+    {
+        private static DiscordSocketClient _client;
+        private static CommandService _commands;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            await _commands.AddModuleAsync<Commands>(null);
+            _client.MessageReceived += HandleCommandAsync;
+            await _client.LoginAsync(TokenType.Bot, "BotTokenHere");
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                  message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+            var context = new SocketCommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, null);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+    }
+
+    public class Commands : ModuleBase
+    {
+        [Command("prices")]
+        public async Task PricesAsync()
+        {           
+                string itemTypes = "any";
+                string query = "query GetItems { Items: items(types: [" + itemTypes + "]) { id name avg24hPrice sellFor{ price source } } }";
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://api.tarkov.dev/graphql");
+                request.Content = new StringContent(JsonConvert.SerializeObject(new { query }), Encoding.UTF8, "application/json");
+                var client = new HttpClient();
+            try
+            {
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                // Deserialize and parse the response from the API into a JObject
+                var json = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                // Select the desired information from the response and order it by highest price
+                var outputList = json["data"]["Items"]
+                .Select(item => new
+                {
+                    id = item["id"],
+                    highestPrice = item["sellFor"].Select(i => (int)i["price"]).Concat(new[] { (int)item["avg24hPrice"] }).Max()
+                })
+                .OrderByDescending(item => item.highestPrice)
+                .ToList();
+                var sb = new StringBuilder();
+                using (var outFile = new StreamWriter("24HourAveragePrices.txt"))
+                {
+                    // Iterate through each item in the outputList
+                    foreach (var item in outputList)
+                    {
+                        // Write a string "set_price " followed by the item's id and highestPrice to the file
+                        outFile.WriteLine("set_price " + item.id + " " + item.highestPrice);
+                    }
+                }
+                var file = File.OpenRead("24HourAveragePrices.txt");
+                var memoryStream = new MemoryStream();
+                file.CopyTo(memoryStream);               
+                memoryStream.Position = 0;
+                await Context.Channel.SendFileAsync(memoryStream, "24HourAveragePrices.txt");
+                file.Dispose();
+                memoryStream.Dispose();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                await ReplyAsync("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Avalanche Price Bot/Avalanche Price Bot/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Avalanche Price Bot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Avalanche Price Bot")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("70f32b65-7815-4c06-8ed0-5f682b5e8b9a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
